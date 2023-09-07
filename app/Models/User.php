1b192b5250b059cb291182936af5d9b7a70cdca1
MNafisN <naf1s.n4ufally@gmail.com>
2023-09-07 15:12:56 +0700

    setup user table and JWT authorization

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
+
+    protected $table = 'users';
 
     /**
      * The attributes that are mass assignable.
@@ -18,7 +21,6 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
         'email',
         'password',
     ];
@@ -41,4 +43,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getJWTCustomClaims()
+    {
+        return[];
+    }
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
 }
